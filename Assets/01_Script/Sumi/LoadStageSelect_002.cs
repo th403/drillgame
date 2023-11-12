e2f9158850fbb396279b5b089a0fd76a4d0e14e1
寺島 <terashit.tomato@gmail.com>
2023-11-12 20:14:20 +0900

    タイトル用のシーンとかの編集

---
+++ Assets/01_Script/Sumi/LoadStageSelect_002.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadStageSelect_002: MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void OnStartPanel()
+    {
+
+            SceneManager.LoadScene("03_StageSelect");
+
+    }
+}
