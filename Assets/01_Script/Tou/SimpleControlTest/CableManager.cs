ea2066a8d7c13cad61f0511fe5f1da05adfa0629
VVLtowl <86180375+VVLtowl@users.noreply.github.com>
2023-11-16 20:49:02 +0900

    create scene of clear perform

--- Assets/01_Script/Tou/SimpleControlTest/CableManager.cs
+++ Assets/01_Script/Tou/SimpleControlTest/CableManager.cs
@@ -16,9 +16,13 @@ public class CableManager : MonoBehaviour
         get { return instance; }
     }
 
+    [Header("attach")]
     public GameObject cablePrefab;
-    public float unitMaxLength;
     public Transform catTransform;
+
+    [Header("edit")]
+    public float unitMaxLength;
+    public float groundDist;
 
     [Header("read only")]
     public List<Transform> cables;
@@ -32,6 +36,26 @@ public class CableManager : MonoBehaviour
     void AddFirstCable()
     {
         AddCable(catTransform.position);
+    }
+
+    public void SetUpCable()
+    {
+        //make ray
+        Ray ray = new Ray(catTransform.position, Vector3.down);
+        LayerMask layer = LayerMask.GetMask("Terrain");
+        RaycastHit hit;
+        Vector3 endPos;
+        if(Physics.Raycast( ray, out hit, groundDist, layer))
+        {
+            endPos = hit.point;
+        }
+        else
+        {
+            endPos = catTransform.position + Vector3.down * groundDist;
+        }
+        Vector3 dir = (endPos - currentCable.transform.position).normalized;
+        currentCable.joint.forward = dir;
+
     }
 
     public void AddCable(Vector3 pos)
