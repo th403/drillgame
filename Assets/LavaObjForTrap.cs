c6cb333c401986d6a2dbd6f93810b814d856f069
Keypad624 <105464238+Keypad624@users.noreply.github.com>
2023-12-15 15:53:08 +0900

    Trap Effect &SE Ddebug

--- Assets/LavaObjForTrap.cs
+++ Assets/LavaObjForTrap.cs
@@ -6,7 +6,6 @@ public class LavaObjForTrap : MonoBehaviour
 {
     public GameObject Effect;
     public int Damage=3000;
-    public AudioSource SELavaDestroy;
 
     // Start is called before the first frame update
     void Start()
@@ -28,8 +27,6 @@ public class LavaObjForTrap : MonoBehaviour
             {
                 Instantiate(Effect, transform.position, transform.rotation);
             }
-            //SoundManger.Instance.PlaySELavaDrop();
-            SELavaDestroy.Play();
             Destroy(this.gameObject);
         }
         if (other.tag == "Player")
@@ -40,8 +37,6 @@ public class LavaObjForTrap : MonoBehaviour
                 clone.transform.localScale = transform.localScale;
             }
             EventCtrl.Instance.PlayerGetMoney(-Damage);
-            //SoundManger.Instance.PlaySELavaDrop();
-            SELavaDestroy.Play();
             Destroy(this.gameObject);
         }
 
