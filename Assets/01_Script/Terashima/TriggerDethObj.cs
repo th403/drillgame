4ecf1a375d3403f42ea82e9e8f9355b329a2be64
寺島 <terashit.tomato@gmail.com>
2023-12-28 02:50:41 +0900

    新しいEnergyプレハブを作ったり

---
+++ Assets/01_Script/Terashima/TriggerDethObj.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDethObj : MonoBehaviour
+{
+    public GameObject Effect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if (other.tag == "Driller"&& Effect)
+        if ((other.tag == "Driller" || other.tag == "Player") && Effect)
+        {
+            Instantiate(Effect, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+        }
+
+    }
+
+}
